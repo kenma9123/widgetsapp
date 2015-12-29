32a6334dbaf5fbca14e522ffec4e2eedf637f425
Kenneth Palaganas <kenma9123@gmail.com>
2015-12-29 19:11:54 +0800

    initial ajax setup

---
+++ server/classes/Ajax.php
@@ -0,0 +1,38 @@
+<?php
+
+class Ajax extends AjaxHandler
+{
+    public $httpresponse;
+
+    public function setHeaders() {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST');
+        header('Access-Control-Allow-Headers: Content-Type');
+    }
+
+    public function test() {
+        $this->success('Hello word');
+    }
+
+    public function lists3Objects() {
+        try {
+            $client = new S3Client();
+            $result = $client->listObjects(array(
+
+            ));
+            $this->success($result);
+        } catch (S3Exception $e) {
+            $this->error($e->getMessage());
+        } catch (AwsException $e) {
+            // This catches the more generic AwsException. You can grab information
+            // from the exception using methods of the exception object.
+            $this->error(array(
+                $e->getAwsRequestId(),
+                $e->getAwsErrorType(),
+                $e->getAwsErrorCode()
+            ));
+        }
+    }
+}
+
+?>
